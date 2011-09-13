cc77ea3e9a11901d2382698ae0798dfbd67925fa
Marek Jelen <marek@jelen.biz>
2011-09-13 16:11:01 +0200

    Build on linux and on Solaris (at least with gcc)

--- ext/noderb_extension/extconf.rb
+++ ext/noderb_extension/extconf.rb
@@ -1,6 +1,18 @@
 require "mkmf"
 
-`cd libuv; make; cd ..; cp libuv/uv.a libuv.a`
+cflags = " -shared "
+
+if $solaris
+  cflags = " -G -fPIC "
+  # From EventMachine
+  CONFIG['LDSHARED'] = "$(CXX) -G -fPIC"
+  if CONFIG['CC'] == 'cc'
+    $CFLAGS = CONFIG['CFLAGS'] = "-g -O2 -fPIC"
+    CONFIG['CCDLFLAGS'] = "-fPIC"
+  end
+end
+
+`cd libuv; CFLAGS="#{cflags}" make; cd ..; cp libuv/uv.a libuv.a`
 
 dir_config("uv", File.expand_path("../libuv/include", __FILE__), File.expand_path("../libuv", __FILE__))
 
